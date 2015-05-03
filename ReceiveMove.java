8aba1f0ded5a754907409579ed5b8bd4309a5d90
Carlos Schneider <carlosaschjr@gmail.com>
2015-05-03 20:12:30 -0300

    3rd Try

--- ReceiveMove.java
+++ ReceiveMove.java
@@ -8,33 +8,19 @@ public class ReceiveMove implements Runnable{
 
 	public Server s;
 	public int player;
-	public Scanner scan;
-	public PrintWriter pw;
 
 	public ReceiveMove(Server s, int player)throws IOException{
 		this.s = s;
 		this.player = player;
-		switch (player){
-			case 1:
-				this.scan = s.scan1;
-				this.pw = s.pw1;
-				break;
-			case 2:
-				this.scan = s.scan2;
-				this.pw = s.pw2;
-				break;
-			default:
-				break;
-		}
 	}
 
 	@Override
 	public void run() {
 
 		String input;
-		while (scan.hasNextLine()) {
-			input = scan.nextLine();
-			pw.println(input);
+		while (s.scan[player].hasNextLine()) {
+			input = s.scan[player].nextLine();
+			s.pw[player].println(input);
 			System.out.println("Player " + player + " : " + input);
 		}
 
--- Server.java
+++ Server.java
@@ -1,7 +1,6 @@
 package chatjava;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
